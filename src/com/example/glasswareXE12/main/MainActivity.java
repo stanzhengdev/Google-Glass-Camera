dd79a90dd7935591fe824b67c54288ad285a667d
stanzheng <dev.stanzheng@gmail.com>
2014-02-25 01:46:53 -0500

    sync branch

--- src/com/example/glasswareXE12/main/MainActivity.java
+++ src/com/example/glasswareXE12/main/MainActivity.java
@@ -1,31 +1,57 @@
 package com.example.glasswareXE12.main;
 
 import java.io.File;
+import java.io.IOException;
 
 import com.google.android.glass.app.Card;
 import com.google.android.glass.media.CameraManager;
-import com.google.android.glass.touchpad.GestureDetector;
+import com.google.android.glass.touchpad.*;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.gesture.Gesture;
+import android.hardware.*;
 import android.view.KeyEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.*;
+import android.hardware.Camera.PictureCallback;
+import android.content.Intent;
 import android.os.FileObserver;
 import android.provider.MediaStore;
 
+
 public class MainActivity extends Activity {
+	
+// create variable instances
+	
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	private GestureDetector mGestureDetector;
+
+	private Camera mCamera;
+	private CameraPreview mPreview;
+	
+
 	protected void onCreate(Bundle savedInstanceState){	
 		super.onCreate(savedInstanceState);
-		
+		//create gesture detector 
+	    //mGestureDetector = createGestureDetector(this);
 		Card card1 = new Card(this);
 		card1.setText("Hello World!");
 		card1.setFootnote("Footer Note ...");
 		View card1View = card1.toView();
-		
+	    //Calling the intent
 		setContentView(card1View);
+		takePicture();
+//	    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+//	    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, false);       
+//	    startActivityForResult(cameraIntent, 1);
 		
 	}
 	@Override
@@ -40,16 +66,22 @@ public class MainActivity extends Activity {
 	    }
 	}
 
+	
+	
+	/*** boiler plate google code 
+	 * https://developers.google.com/glass/develop/gdk/media-camera/camera
+	 */
 	@Override
 	protected void onResume() {
 	    super.onResume();
 	    // Re-acquire the camera and start the preview.
 	}
 	private static final int TAKE_PICTURE_REQUEST = 1;
-
-	private void takePicture() {
+	
+	public void takePicture() {
 	    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    startActivityForResult(intent, TAKE_PICTURE_REQUEST);
+	    
 	}
 
 	@Override
@@ -106,5 +138,89 @@ public class MainActivity extends Activity {
 	        };
 	        observer.startWatching();
 	    }
+	}
+	/**
+	 * end of boiler plate 
+	 */
+	private class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+	    private SurfaceHolder mHolder;
+	    private Camera mCamera;
+
+	    public CameraPreview(Context context, Camera camera) {
+	        super(context);
+	        mCamera = camera;
+
+	        Log.v(TAG,"In CameraPreview");
+
+	        // Install a SurfaceHolder.Callback so we get notified when the
+	        // underlying surface is created and destroyed.
+	        mHolder = getHolder();
+
+	        Log.v(TAG,"Got holder");
+
+	        mHolder.addCallback(this);
+
+	        Log.v(TAG,"Added callback");
+
+	        // deprecated setting, but required on Android versions prior to 3.0
+	        //mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	    }
+
+	    public void surfaceCreated(SurfaceHolder holder) {
+	        // The Surface has been created, now tell the camera where to draw the preview.
+	        try {
+	            Log.v(TAG,"in surface created");
+	            mCamera.setPreviewDisplay(holder);
+	            Log.v(TAG,"set preview display");
+	            mCamera.startPreview();
+	            Log.v(TAG,"preview started");
+	        } catch (IOException e) {
+	            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+	        }
+	    }
+
+	    public void surfaceDestroyed(SurfaceHolder holder) {
+	        // empty. Take care of releasing the Camera preview in your activity.
+	        mCamera.stopPreview();
+	        mCamera.release();
+	        mCamera = null;
+	    }
+
+	    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+	        // If your preview can change or rotate, take care of those events here.
+	        // Make sure to stop the preview before resizing or reformatting it.
+
+	        Log.v(TAG,"in surface changted");
+
+	        if (mHolder.getSurface() == null){
+	          // preview surface does not exist
+	            Log.v(TAG,"surface don't exist");
+	          return;
+	        }
+
+	        // stop preview before making changes
+	        try {
+	            mCamera.stopPreview();
+	            Log.v(TAG,"stopped preview");
+	        } catch (Exception e){
+	          // ignore: tried to stop a non-existent preview
+	            Log.v(TAG,"preview e");
+	        }
+
+	        // start preview with new settings
+	        try {
+	            Log.v(TAG,"startpreview");
+	            mCamera.setPreviewDisplay(mHolder);
+	            mCamera.startPreview();
+
+	        } catch (Exception e){
+	            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+	        }
+	    }
+	}
+
+	public void takePic() {
+	    // get an image from the camera
+	    mCamera.takePicture(null, null, mPicture);
 	}
 }
